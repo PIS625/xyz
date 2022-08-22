61535698d0756f1afab3a81c60d0ee1989a532e3
PIS625 <107376034+PIS625@users.noreply.github.com>
2022-08-22 17:45:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ birthday ='07-23'
 app_id = 'wx2813e8d9392dc20b'
 app_secret = 'bf2060150f6f645960b9481b7a6f52b2'
 
-user_id = 'ojrgE5z_CQg8W4w5iNr6cY_CWdtw'
+user_id = 'ojrgE583R36QChNqX_W-C-2FL7Io'
 template_id = 'r2bUtAL1Cc2GyG3BQMCC_YSil8-Wk3nimGK6Bs7xxFE'
 
 
